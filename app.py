70a00abc3bae0e8c5c0cbc638efdc0d48e0c582f
bf0040792-rgb <bf0040792@gmail.com>
2026-01-27 17:57:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 active_tasks = {}
 
 def send_bomber(mobile):
-    count = 2
+    count = 1
     session = requests.Session()
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36",
@@ -79,7 +79,7 @@ def send_bomber(mobile):
         except: pass
 
         print(f"Round {count} Finished.")
-        count += 15
+        count += 1
         time.sleep(1) # Safe gap taaki saari APIs block na hon
 
 @app.route('/')
