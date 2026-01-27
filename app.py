53b7cc48071d70a8e47997bed35b48f5c5b79b61
bf0040792-rgb <bf0040792@gmail.com>
2026-01-27 21:41:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def send_bomber(mobile):
 
         print(f"Round {count} Finished.")
         count += 1
-        time.sleep(3) # Safe gap taaki saari APIs block na hon
+        time.sleep(15) # Safe gap taaki saari APIs block na hon
 
 @app.route('/')
 def index():
