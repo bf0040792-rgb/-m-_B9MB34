eb094160d6fda6170336c46e82a2b574ff4cb97e
bf0040792-rgb <bf0040792@gmail.com>
2026-09-19 15:44:54 -1200

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def send_bomber(mobile):
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36",
         "Accept": "*/*"
     }
-     apis = [
+
+    apis = [
     {
         "name": "confirmtkt",
         "method": "POST",
